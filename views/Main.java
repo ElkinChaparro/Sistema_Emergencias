591619ee2aa4388cfb99aab876983befa2253a1f
Elkin Humberto Chaparro Franklin <elkinchaparro@gmail.com>
2025-01-30 00:38:59 -0500

    Create DailyRequest

--- views/Main.java
+++ views/Main.java
@@ -1,6 +1,30 @@
 package views;
+
+import java.util.Scanner;
+
+import drivers.*;
+import images.*;
+//import models.*;
+
 public class Main {
+
+    static boolean exit = false;
+    static Scanner scGame = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+
+        imagesACCI.Welcome();
+        dailyRequest.Application();
+        scGame.nextLine();
+        do {
+            // TODO comenzar codigo para menu de inicio
+
+            var notify = dailyRequest.ambulances.get(0);
+            int sum = notify + 15;
+            System.out.println("Es " + sum);
+
+            // Salida de emergencia
+            exit = true;
+        } while (exit = true);
     }
 }
